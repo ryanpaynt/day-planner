7e2ab058bad0e119d77f7c789df961e1f8102282
Ryan <ryanpaynt@uncc.edu>
2021-05-24 16:46:03 -0400

    finished js. My local storage is now in effect for all mid-cols

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,15 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 var root = $('#root');
-var edit = $('#midcol');
+var edit1 = $('#midcol1');
+var edit2 = $('#midcol2');
+var edit3 = $('#midcol3');
+var edit4 = $('#midcol4');
+var edit5 = $('#midcol5');
+var edit6 = $('#midcol6');
+var edit7 = $('#midcol7');
+var edit8 = $('#midcol8');
+var edit9 = $('#midcol9');
 var date = $('#currentDay');
 var row = $('.row');
 var unix = parseInt(moment().format('HH'));
@@ -21,12 +29,60 @@ var obj = [];
 
 $('#currentDay').text(moment().format("MMM Do, LTS"));
 
-edit.text(localStorage.getItem('edit'));
+edit1.text(localStorage.getItem('edit1'));
 
-    edit.on('blur', function(){
-        localStorage.setItem('edit', this.innerHTML);
+    edit1.on('blur', function(){
+        localStorage.setItem('edit1', this.innerHTML);
     });
-var i = 0;
+
+edit2.text(localStorage.getItem('edit2'));
+
+    edit2.on('blur', function(){
+        localStorage.setItem('edit2', this.innerHTML);
+    });
+
+edit3.text(localStorage.getItem('edit3'));
+
+    edit3.on('blur', function(){
+        localStorage.setItem('edit3', this.innerHTML);
+    });
+
+edit4.text(localStorage.getItem('edit4'));
+
+    edit4.on('blur', function(){
+        localStorage.setItem('edit4', this.innerHTML);
+    });
+
+edit5.text(localStorage.getItem('edit5'));
+
+    edit5.on('blur', function(){
+        localStorage.setItem('edit5', this.innerHTML);
+    });
+
+edit6.text(localStorage.getItem('edit6'));
+
+    edit6.on('blur', function(){
+        localStorage.setItem('edit6', this.innerHTML);
+    });
+
+edit7.text(localStorage.getItem('edit7'));
+
+    edit7.on('blur', function(){
+        localStorage.setItem('edit7', this.innerHTML);
+    });
+
+edit8.text(localStorage.getItem('edit8'));
+
+    edit8.on('blur', function(){
+        localStorage.setItem('edit8', this.innerHTML);
+    });
+
+edit9.text(localStorage.getItem('edit9'));
+
+    edit9.on('blur', function(){
+        localStorage.setItem('edit9', this.innerHTML);
+    });
+
 $('[id*=num]').each(function(){
     obj.push(`${parseInt(this.innerHTML)}`);
 });
