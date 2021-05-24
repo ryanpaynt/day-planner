2f7f848a7e2951d8cf12992176589af3a10a8dfe
Ryan <ryanpaynt@uncc.edu>
2021-05-24 16:37:32 -0400

    html is finished, made sure it was suitable for DOM traversal. JS file is ready for color coding, but still need to enable local storage on the remaining times

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,38 @@
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
+var root = $('#root');
+var edit = $('#midcol');
+var date = $('#currentDay');
+var row = $('.row');
+var unix = parseInt(moment().format('HH'));
+
+var obj = [];
+
+$('#currentDay').text(moment().format("MMM Do, LTS"));
+
+edit.text(localStorage.getItem('edit'));
+
+    edit.on('blur', function(){
+        localStorage.setItem('edit', this.innerHTML);
+    });
+var i = 0;
+$('[id*=num]').each(function(){
+    obj.push(`${parseInt(this.innerHTML)}`);
+});
+console.log(obj);
+
+checkTime();
+
+var i = 7;
+function checkTime(){
+    for(var i = 0; i < obj.length; i++){
+        if(unix === parseInt(root.children().eq(i).children().eq(0).text().substring(0,2))){
+            root.children().eq(i).children().eq(1).css('background-color', 'brown');
+        } else if(unix < parseInt(root.children().eq(i).children().eq(0).text().substring(0,2))){
+            root.children().eq(i).children().eq(1).css('background-color', 'green');
+        }else {
+            root.children().eq(i).children().eq(1).css('background-color', 'red');
+        }
+    }
+}
